e324141aa595b4018aab67169769cf0833793f97
DanL <unspecified@email.com>
2021-08-07 21:09:09 +0100

    Added basic 'game'.
    This 'game' is simply a satellite map that pans to your location every
    two seconds (highlighted with the blue circle).

---
+++ app/www/js/game.js
@@ -0,0 +1,34 @@
+var map;
+
+var accuracyCircle;
+
+function setupMap() {
+	map = L.map('map');
+	L.tileLayer("http://" + serverIP + "/tile?x={x}&y={y}&z={z}", {
+		//Standard settings for mapbox (which we're using for the forseeable future).
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    	maxZoom: 18,
+    	tileSize: 512,
+		zoomOffset: -1
+	}).addTo(map);
+	map.on('locationfound', onLocationFound);
+	window.setInterval(updateLoc, 2000);
+}
+
+function updateLoc(){
+	map.locate({setView: true, maxZoom: 16});
+}
+
+//Copy-pasted from Leaflet example code, then modified to suit me.
+function onLocationFound(e) {
+	console.log(e);
+    var radius = e.accuracy;
+	//Delete old accuracy circle
+	if (accuracyCircle !== undefined) {
+		map.removeLayer(accuracyCircle);
+	}
+	//Create new one.
+    accuracyCircle = L.circle(e.latlng, radius).addTo(map);
+}
+
+window.setTimeout(setupMap, 200); //Set a small timeout to allow everything to load.
