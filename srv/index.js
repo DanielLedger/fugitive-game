4af4bf8c1efe395b1a23f43bf99c362ddf00531b
DanL <unspecified@email.com>
2021-08-11 13:41:29 +0100

    Added (currently broken) SSL support.
    Currently errors with "SSL_NO_CIPHER_OVERLAP".

--- srv/index.js
+++ srv/index.js
@@ -1,4 +1,5 @@
 const config = require('config');
+
 const port = config.get("Server.Port");
 
 //Get express and initialise it.
@@ -8,6 +9,20 @@ const app = express();
 //Import additional routes defined in other files.
 require('./maproutes').init(config, app);
 
-app.listen(port, () => {
-	console.log("Application started on port " + port + ".");
-});
+if (!config.get('Server.SSL.Enabled')){
+	app.listen(port, () => {
+		console.log("Application started on port " + port + ".");
+	});	
+}
+else {
+	//Set up SSL (since SSL must be provided one way or another, there will be no insecure port).
+	const https = require('https');
+	const fs = require('fs');
+	var key = fs.readFileSync(config.get('Server.SSL.Key'));
+	var cert = fs.readFileSync(config.get('Server.SSL.Cert'));
+	
+	var srv = https.createServer({key: key, cert: cert, ciphers: "DEFAULT:!SSLv2:!RC4:!EXPORT:!LOW:!MEDIUM:!SHA1"}, app);
+	srv.listen(port, () => {
+		console.log("HTTPS application started on port " + port + ".");
+	});	
+}
