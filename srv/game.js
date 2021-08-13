30ac65188f991b16938949ed229798d7eaee59d5
DanL <unspecified@email.com>
2021-08-13 12:40:11 +0100

    Added game class.
    Performs the basic websocket management.

---
+++ srv/game.js
@@ -0,0 +1,56 @@
+const uuid = require('uuid');
+
+class Game {
+	
+	constructor(){
+		this.players = {}; //Maintains a list of unique ids -> websocket sessions.
+		this.sessionRevLookup = {} //A list of websockets -> ids.
+		this.publicIDS = {} //A list of unique IDs -> public IDs that we send to the end clients (given that the ids here are also connection secrets).
+	}
+	
+	initSession(){
+		//Creates a session for a new player. Returns them a unique ID for their session.
+		var newID = uuid.v4();
+		//Since they don't currently have a socket, add them as 'null' (NOT undefined, since that means "session isn't valid")
+		this.players[newID] = null;
+		//Generate them a new public ID.
+		this.publicIDS[newID] = uuid.v4();
+		return newID;
+	}
+	
+	transferSession(playerID, newSession){
+		//Deletes this player's old session and makes the 'newSession' their current one.
+		if (this.players[playerID] === undefined){
+			//Player is not registered, do not transfer them a session and return false.
+			return false;
+		}
+		else {
+			//Player's current session is now the new one.
+			this.players[playerID] = newSession;
+			this.sessionRevLookup[newSession] = playerID;
+			//Set up the new session with a handler.
+			newSession.onmessage = (msg) => {
+				this.handleWSMessage(playerID, msg);
+			};
+		}
+	}
+	
+	closeSession(sess){
+		//Renders a websocket session invalid.
+		delete this.sessionRevLookup[sess];
+	}
+	
+	//The big method which powers a lot of the core functionailty of the game: this method controls the handling of the incoming websocket messages.
+	handleWSMessage(sess, msg){
+		console.log("WS message from " + sess + ": " + msg);
+		//Echo it to all connected clients (except the one that sent it, they don't care).
+		var msg = this.publicIDS[this.sessionRevLookup[sess]] + ":" + msg;
+		for (var ws of this.players){
+			if (ws !== sess){
+				ws.send(msg);
+			}
+		}
+	}
+}
+
+module.exports.Game = Game;
