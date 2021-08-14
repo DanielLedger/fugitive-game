fb32482e1f878d344a72fcd0bf48337fcd3471ce
DanL <unspecified@email.com>
2021-08-14 16:11:46 +0100

    Added basic websocket functions.
    Now connects to the socket.

--- app/www/js/checkserver.js
+++ app/www/js/checkserver.js
@@ -5,4 +5,19 @@ if (serverIP === null) {
 	document.location = "index.html"
 }
 
-//TODO: Actually check if the server IP is valid or not.
+//Contact the server and try to get a websocket.
+var code = window.sessionStorage.getItem('GameCode'); //Identifies the game.
+var uuid = window.sessionStorage.getItem('ID'); //Identifies the us.
+
+if (code === null || uuid === null){
+	//Not got two bits of required info.
+	document.location = "index.html";
+}
+
+//Try and make a websocket connection.
+var wsIP = serverIP.replace(/https?/, "ws"); //Replace http or https with ws (because it's a wesocket).
+const gameSocket = new WebSocket(`${wsIP}/game?code=${code}&uuid=${uuid}`);
+gameSocket.onclose = () => {
+	//If closed by not us, that probably means our credentials are wrong.
+	document.location = "index.html";
+};
