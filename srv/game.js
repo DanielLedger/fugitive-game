3c96f2a05026351d18411c8b0c2a0e8ea6b07257
DanL <unspecified@email.com>
2021-08-15 10:19:54 +0100

    Fixed reference errors.
    Websocket messages are now attributed to the correct client.

--- srv/game.js
+++ srv/game.js
@@ -4,7 +4,6 @@ class Game {
 	
 	constructor(){
 		this.players = {}; //Maintains a list of unique ids -> websocket sessions.
-		this.sessionRevLookup = {} //A list of websockets -> ids.
 		this.publicIDS = {} //A list of unique IDs -> public IDs that we send to the end clients (given that the ids here are also connection secrets).
 	}
 	
@@ -20,6 +19,7 @@ class Game {
 	
 	transferSession(playerID, newSession){
 		//Deletes this player's old session and makes the 'newSession' their current one.
+		console.log("Before players: " + JSON.stringify(this.players));
 		if (this.players[playerID] === undefined){
 			//Player is not registered, do not transfer them a session and return false.
 			return false;
@@ -27,30 +27,33 @@ class Game {
 		else {
 			//Player's current session is now the new one.
 			this.players[playerID] = newSession;
-			this.sessionRevLookup[newSession] = playerID;
+			newSession.playerID = playerID; //This may or may not work.
 			var currentGame = this; //Required due to scope problems.
 			//Set up the new session with a handler.
+			var ws = newSession;
 			newSession.onmessage = (msg) => {
-				currentGame.handleWSMessage(newSession, msg, currentGame);
+				currentGame.handleWSMessage(ws, msg, currentGame);
 			};
 			//Also set it up with a close handler.
 			newSession.onclose = () => {
-				currentGame.closeSession(this); //This is why we need a 'currentGame' reference: 'this' refers to the websocket.
-			}
+				currentGame.closeSession(ws); //This is why we need a 'currentGame' reference: 'this' refers to the websocket.
+			};
+			
+			console.log("After players: " + JSON.stringify(this.players));
 			return true;
 		}
 	}
 	
 	closeSession(sess){
 		//Renders a websocket session invalid.
-		delete this.sessionRevLookup[sess];
+		sess.playerID = undefined;
 	}
 	
 	//The big method which powers a lot of the core functionailty of the game: this method controls the handling of the incoming websocket messages.
 	handleWSMessage(sess, msg, game){
-		console.log("WS message from " + game.sessionRevLookup[sess] + ": " + msg.data);
+		console.log("WS message from " + sess.playerID + ": " + msg.data);
 		//Echo it to all connected clients (except the one that sent it, they don't care).
-		var msg = game.publicIDS[game.sessionRevLookup[sess]] + ":" + msg.data;
+		var msg = game.publicIDS[sess.playerID] + ":" + msg.data;
 		for (var ws of Object.values(game.players)){
 			if (ws === null){
 				continue; //Race condition kinda.
