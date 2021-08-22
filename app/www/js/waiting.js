9343c3574c882060cfe6d28695010100a7edbd54
DanL <unspecified@email.com>
2021-08-22 12:43:13 +0100

    Added basic info display.

---
+++ app/www/js/waiting.js
@@ -0,0 +1,23 @@
+function showGameStatus(json){
+	var giObj = JSON.parse(json);
+	//TODO: Have a nice looking bar here
+	document.getElementById("wsping").innerHTML = `<span class='h4'>Websocket last ping: </span><span class='h5'>${Date.now()}</span>`;
+	if (giObj.host){
+		document.getElementById("ishost").innerHTML = `<span class='h4'>Host: </span><a class='btn btn-success'>Yes</a>`;
+	}
+	else {
+		document.getElementById("ishost").innerHTML = `<span class='h4'>Host: </span><a class='btn btn-danger'>No</a>`;
+	}
+	document.getElementById("players").innerHTML = `<span class='h4'>Players: </span><span class='h5'>${giObj.players}</span>`;
+}
+
+gameSocket.addEventListener('message', (m) => {
+	if (m.data.startsWith('INFO')){
+		var json = m.data.split(' ')[1];
+		showGameStatus(json);
+	}
+});
+
+window.setInterval(() => {
+	gameSocket.send("GAMEINFO");
+}, 2000);
