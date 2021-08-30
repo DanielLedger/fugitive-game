3b798b09977b51dc7f3261fa0fc65571a2fab8cb
DanL <unspecified@email.com>
2021-08-30 11:17:36 +0100

    Added role assign button.

--- app/www/js/waiting.js
+++ app/www/js/waiting.js
@@ -4,9 +4,12 @@ function showGameStatus(json){
 	document.getElementById("wsping").innerHTML = `<span class='h4'>Websocket last ping: </span><span class='h5'>${Date.now()}</span>`;
 	if (giObj.host){
 		document.getElementById("ishost").innerHTML = `<span class='h4'>Host: </span><a class='btn btn-success'>Yes</a>`;
+		//Show the 'allocate roles' button
+		document.getElementById('lockrolesselection').style = "display: block;";
 	}
 	else {
 		document.getElementById("ishost").innerHTML = `<span class='h4'>Host: </span><a class='btn btn-danger'>No</a>`;
+		document.getElementById('lockrolesselection').style = "display: none;";
 	}
 	document.getElementById("players").innerHTML = `<span class='h4'>Players: </span><span class='h5'>${giObj.players}</span>`;
 	//TODO: Render this in a CASE-SENSITIVE font.
@@ -23,3 +26,10 @@ gameSocket.addEventListener('message', (m) => {
 window.setInterval(() => {
 	gameSocket.send("GAMEINFO");
 }, 2000);
+
+document.getElementById('lockrolesselection').onclick = () => {
+	//Disable the button (so it can't be clicked again
+	document.getElementById('lockrolesselection').disabled = true;
+	//Send the assign roles message.
+	gameSocket.send("ROLE_ASSIGN");
+};
