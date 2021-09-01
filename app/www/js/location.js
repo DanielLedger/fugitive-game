b92edc1a23ed5f0a38c5765214b615343373b1b4
DanL <unspecified@email.com>
2021-09-01 13:38:47 +0100

    Added new 'location library' to make using background location easier.

---
+++ app/www/js/location.js
@@ -0,0 +1,59 @@
+const Geolocation = {};
+
+function getGeolocationService(){
+	//Gets the geolocation service which will either use BackgroundGeolocation (if available) or navigator.geolocation
+	if (BackgroundGeolocation !== undefined) {
+		//We have the ability to use background geolocation.
+		Geolocation.watch = (timeDelay, callback) => {
+			//Setup the configuration.
+			BackgroundGeolocation.configure({
+				locationProvider: BackgroundGeolocation.ACTIVITY_PROVIDER,
+    			desiredAccuracy: BackgroundGeolocation.HIGH_ACCURACY,
+    			stationaryRadius: 5,
+    			distanceFilter: 5,
+    			notificationTitle: 'Fugitive Game',
+    			notificationText: 'The game is running in the background. Check the map for updates.',
+    			startForeground: true,
+				debug: true,
+    			interval: timeDelay,
+    			fastestInterval:timeDelay,
+    			activitiesInterval: timeDelay
+			});
+			
+			//User permission stuff
+			BackgroundGeolocation.on('authorization', function(status) {
+    			console.log('[INFO] BackgroundGeolocation authorization status: ' + status);
+    			if (status !== BackgroundGeolocation.AUTHORIZED) {
+     			// we need to set delay or otherwise alert may not be shown
+      			setTimeout(function() {
+        			var showSettings = confirm('App requires location tracking permission. Would you like to open app settings?');
+        			if (showSettings) {
+          				return BackgroundGeolocation.showAppSettings();
+        			}
+      				}, 1000);
+    			}
+  			});
+			//Bind the given function to the location event.
+			BackgroundGeolocation.on('location', (loc) => {
+				//We're going to run it as a task to be on the safe side (not that I'm able to build for iOS).
+				BackgroundGeolocation.startTask((tk) => {
+					callback(loc);
+					BackgroundGeolocation.endTask(tk);
+				});
+			});
+			
+			BackgroundGeolocation.start(); //Start watching.
+		};
+		
+		Geolocation.stop = () => {
+			BackgroundGeolocation.stop();
+		};
+	}
+	else if (window.navigator !== undefined){
+		//use browser inbuilt navigator. Currently not implemented.
+		console.error("Navigation using the browser 'navigation' object is not currently not supported!");
+		Geolocation.watch = () => {};
+		Geolocation.stop = () => {};
+	}
+	return Geolocation;
+}
