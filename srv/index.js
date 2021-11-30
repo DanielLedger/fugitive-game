dff5ee1acc8ed9273ac63ca1aee72558325a1f66
BuildTools <unconfigured@null.spigotmc.org>
2021-11-30 13:13:42 +0000

    Allow socket.io to be used.
    It is currently pointless though.

--- srv/index.js
+++ srv/index.js
@@ -20,8 +20,17 @@ app.set('trust proxy', 'loopback');
 //Load and setup ExpressWS. This currently won't work with HTTPS.
 require('express-ws')(app);
 
+//Load and setup socket.io
+const { Server } = require('socket.io');
+const { createServer } = require("http");
+
+var httpSrv = createServer(app);
+
+const io = new Server(httpSrv);
+
 //Load game class.
 const Game = require('./game').Game;
+const { create } = require('domain');
 
 //All currently running games
 var games = {};
@@ -136,6 +145,11 @@ app.ws('/game', (ws, req) => {
 	}
 })
 
+io.on('connection', (sock) => {
+	sock.send("Hello World!");
+	console.log(`Got socket connection from ${sock.id}`);
+})
+
 //Import additional routes defined in other files.
 require('./maproutes').init(config, app);
 require('./locationpost').init(app, games, uuids);
@@ -153,20 +167,5 @@ setInterval(() => {
 	}
 }, 3000); //TODO: Massively increase delay here (but I'm impatient).
 
-if (!config.get('Server.SSL.Enabled')){
-	app.listen(port, () => {
-		console.log("Application started on port " + port + ".");
-	});	
-}
-else {
-	//Set up SSL (since SSL must be provided one way or another, there will be no insecure port).
-	const https = require('https');
-	const fs = require('fs');
-	var key = fs.readFileSync(config.get('Server.SSL.Key'));
-	var cert = fs.readFileSync(config.get('Server.SSL.Cert'));
-	
-	var srv = https.createServer({key: key, cert: cert, ciphers: "DEFAULT:!SSLv2:!RC4:!EXPORT:!LOW:!MEDIUM:!SHA1"}, app);
-	srv.listen(port, () => {
-		console.log("HTTPS application started on port " + port + ".");
-	});	
-}
+//Nobody used the https and it was broken anyway, so ignore.
+httpSrv.listen(port);
